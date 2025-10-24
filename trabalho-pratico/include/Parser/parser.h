324650d3460bbd803da36f61806d108489ece0c7
duarterodrigues.111517 <a111517@alunos.uminho.pt>
2025-10-24 01:10:02 +0100

    parser teste

--- trabalho-pratico/include/Parser/parser.h
+++ trabalho-pratico/include/Parser/parser.h
@@ -1,20 +1,18 @@
-#ifndef PARSER_H
-#define PARSER_H
+#ifndef PARSER_GEN_H
+#define PARSER_GEN_H
 
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "glib.h"
+#include <glib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+// Liberta todos os elementos do GArray sem o destruir
+void free_garray_parsed_elements(GArray *array);
 
+// Processa uma linha de um ficheiro CSV, divide pelos ';' e guarda no GArray
+int process_line(FILE *file, GArray *array, char **lineCopy);
 
+// Liberta todo o GArray e destrói-o
+void free_garray_parsed(GArray *array);
 
-
-
-
-
-
-
-
-
-#endif // PARSER_H
+#endif
