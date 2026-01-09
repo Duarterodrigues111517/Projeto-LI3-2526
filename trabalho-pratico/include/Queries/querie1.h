9f1c6029233c87780032bf397b12ac14aa26665e
Cristiano Gomes <cristiano-gomes@cristiano.gomes>
2026-01-09 00:41:54 +0000

    eliminei q1

--- trabalho-pratico/include/Queries/querie1.h
+++
@@ -1,8 +0,0 @@
-#ifndef QUERIE1_H
-#define QUERIE1_H
-#include "../Airports_Manager/airports_manager.h"
-
-void querie1(const char *args, AirportsManager_t *am, const char *output_path);
-
-#endif
-
