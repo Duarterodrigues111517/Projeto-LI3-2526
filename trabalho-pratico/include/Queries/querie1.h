3ab9053e0d3a64a2ec9f6ef948d7a7f3c61c77cd
Cristiano Gomes <cristiano-gomes@cristiano.gomes>
2026-01-09 00:40:22 +0000

    a

---
+++ trabalho-pratico/include/Queries/querie1.h
@@ -0,0 +1,8 @@
+#ifndef QUERIE1_H
+#define QUERIE1_H
+#include "../Airports_Manager/airports_manager.h"
+
+void querie1(const char *args, AirportsManager_t *am, const char *output_path);
+
+#endif
+
